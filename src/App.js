2a64d51e8d6e94fffc5e4bff91c46dff0d7f4f9e
Kevin Kruelle <kkruelle2@gmail.com>
2021-09-16 14:07:17 -0400

    created images directory and added image to home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from "../src/Components/header";
+import MainImage from "./images/kid_at_computer.jpg";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="mainImage">
+        <img style={{width: "100%"}}src={MainImage} alt="Kid at Computer Coding"/>
+      </div>
     </div>
   );
 }
