eefbd7815743aafd4461d034fb254b2212130d03
Nick <nwestallen@gmail.com>
2021-09-16 14:31:20 -0400

    switches main content to home component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import Header from "../src/Components/header";
-import MainImage from "./images/kid_at_computer.jpg";
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Gallery from './Components/mentorGallery';
+import home from './Components/home';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <Route exact path="/" component={home} />
         <Route path="/gallery" component={Gallery} />
-        <div className="mainImage">
-          <img style={{width: "100%"}}src={MainImage} alt="Kid at Computer Coding"/>
-        </div>
+
       </div>
     </Router>
   );
