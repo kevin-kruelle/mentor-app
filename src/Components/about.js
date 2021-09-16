45ce406e2852eec79d0afbfd7f6f595f105bcb20
Kevin Kruelle <kkruelle2@gmail.com>
2021-09-16 14:39:29 -0400

    added about component

---
+++ src/Components/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './about.css';
+
+function About(){
+
+    return (
+        <div className="container">
+            <div className="titleBox">
+                <p className="title">Strengthening the technology ecosystem by creating mutually beneficial relationships between communities and corporate partners.</p>
+            </div>
+        </div>
+    );
+}
+
+export default About;
