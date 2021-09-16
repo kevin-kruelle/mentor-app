898ea41bb27bfa3eb797f3c0f43151717ad79212
Kevin Kruelle <kkruelle2@gmail.com>
2021-09-16 12:19:41 -0400

    test push

---
+++ src/Components/Mentor Card/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import dummyData from '../../data.js';
+
+function mentorCard(){
+
+    return (
+        <div>
+            <div className="Card">
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default mentorCard;
